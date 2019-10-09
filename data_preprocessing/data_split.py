b60eaf9abde5d7bdff8758b467d4b22c389d3c65
gordongwb <wicspa@gmail.com>
2019-10-09 20:31:13 +0800

    data_split

--- data_preprocessing/data_split.py
+++ data_preprocessing/data_split.py
@@ -10,8 +10,30 @@ from tqdm import tqdm
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
-data_path = os.path.abspath('..')+'/dataset/mal-api-2019/all_analysis_data.csv'
+data_path = os.path.abspath('..')+'/dataset/mal-api-2019/analysis_data_index.csv'
+# data_path = os.path.abspath('..')+'/dataset/mal-api-2019/all_analysis_data.csv'
 label_path = os.path.abspath('..')+'/dataset/mal-api-2019/labels.csv'
-X = pd.read_csv(data_path)
+fast_train = os.path.abspath('..')+'/dataset/fast_train.txt'
+fast_test = os.path.abspath('..')+'/dataset/fast_test.txt'
+print('reading data ...')
+X = pd.read_csv(data_path,header=None)
+Y = pd.read_csv(label_path,header=None)
 print(X.shape)
-print(X[0][0])
+print(Y.shape)
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1, shuffle=True)
+
+with open('fast_train', 'w') as train:
+    len = X_train.shape[0]
+    for i in range(len):
+        text = X_train[i][0]
+        label = Y_train[i][0]
+        train.write('__label__' + label + ' '+text)
+
+with open('fast_test','w') as test:
+    len = X_test.shape[0]
+    for i in range(len):
+        text = X_test[i][0]
+        label = Y_test[i][0]
+        train.write('__label__' + label + ' ' + text)
+
--- test.py
+++ test.py
@@ -3,4 +3,17 @@
 # @Author : kzl 
 # @File : test.py
 
+import pandas as pd
 
+df = pd.DataFrame([9,2,3,5])
+print(df.shape)
+df.to_csv('test.csv',index=False,header=False)
+
+df2 = pd.read_csv('test.csv',header=None)
+print(df2.shape)
+# print(df2)
+len = df2.shape[0]
+print(len)
+for i in range(len):
+    print(i)
+    print(df2.iloc[i][0])
